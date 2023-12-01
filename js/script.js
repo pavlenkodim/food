1675523cccbe16d3f5bbbe12aba8183db5ceecec
pavlenkodim <pavlenkodim@mail.ru>
2023-12-01 16:21:25 +0600

    Modified slider - add dots

--- js/script.js
+++ js/script.js
@@ -305,6 +305,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         next = document.querySelector('.offer__slider-next'),
         prev = document.querySelector('.offer__slider-prev'),
         current = document.querySelector('#current'),
@@ -332,7 +333,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
     slides.forEach(slide => {
         slide.style.width = width;
-    })
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+
+        if (i === 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
 
     next.addEventListener('click', () => {
         if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) { // '500px'
@@ -349,11 +370,8 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        slideCurrent();
+        dotsView();
     });
 
     prev.addEventListener('click', () => {
@@ -371,12 +389,36 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
+        slideCurrent();
+        dotsView();
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            slideCurrent();
+            dotsView();
+        });
+    });
+
+    function slideCurrent () {
         if (slides.length < 10) {
             current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
         }
-    });
+    }
+
+    function dotsView () {
+        dots.forEach(dot => dot.style.opacity = '0.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
 
     /* First option for the slider
     showSlides(slideIndex);
